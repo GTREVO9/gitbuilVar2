224aaba14aae601ba253809c9231c05700d464ca
GTREVO9 <gtr78816@gmail.com>
2017-06-08 00:08:39 +0800

    commit by gitVar2

---
+++ gitVar1/src/com/gitVar1/gitbuild.java
@@ -0,0 +1,13 @@
+package com.gitVar1;
+
+public class gitbuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("AAAAAAAAA");
+	
+	}
+
+}
